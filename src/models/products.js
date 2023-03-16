6d6ef6f77533f650bc650a86370c8d9930084553
samuel carrizo <samuelcarrizot@gmail.com>
2023-03-16 13:01:12 -0300

    se agreo models producto

---
+++ src/models/products.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    thumbnail: { type: String, required: true },
+    code: { type: String, required: true },
+    stock: { type: Number, required: true },
+    status: { type: String, required: true },
+    category: { type: String, required: true },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
